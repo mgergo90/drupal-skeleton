38ebae50a647f46c7ae5c98fe9afd14a0be7cb01
Gary <gergo.nik.2009@gmail.com>
2018-01-18 12:25:05 +0100

    install to web

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -d web ]; then
-    docker-compose exec web /root/.composer/vendor/drush/drush/drush make profile.make.yml -y
+    docker-compose exec web /root/.composer/vendor/drush/drush/drush make profile.make.yml web -y
     &&
     docker-compose exec web chown -R www-data:www-data /var/www/html/web/sites;
 fi;
